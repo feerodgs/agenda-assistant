63c87b29ff26a7734d2449bdcd506ede2d8c8698
Felipe Rodrigues <felipeaj220@gmail.com>
2026-05-08 16:01:26 -0300

    Corrigindo conflitos

--- src/main/java/dev/feliperodrigue/botagendamento/checker/AppointmentChecker.java
+++
@@ -1,7 +0,0 @@
-package dev.feliperodrigue.botagendamento.checker;
-
-import dev.feliperodrigue.botagendamento.domain.AppointmentResult;
-
-public interface AppointmentChecker {
-    AppointmentResult check();
-}
